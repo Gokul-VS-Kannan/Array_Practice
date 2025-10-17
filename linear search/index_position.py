277b809ef0f52e1c7375c9f99ab349dd37baf59d
Gokul-VS-Kannan <gokulvsvikram@gmail.com>
2025-10-18 00:12:10 +0530

    solved the problem

--- linear search/index_position.py
+++ linear search/index_position.py
@@ -1,1 +1,15 @@
 # Write a program to check if a number exists in the list using linear search.
+
+arr_1 = [10,20,30,70,45,5]
+
+target = 20
+
+def check(arr_1,target):
+    n=len(arr_1)
+    for i in range(n):
+        if arr_1[i] == target:
+            print(f"{target} found at index {i}")  
+            break    
+    else:
+        print(f"{target} is not in the given array")  
+check(arr_1,target)
